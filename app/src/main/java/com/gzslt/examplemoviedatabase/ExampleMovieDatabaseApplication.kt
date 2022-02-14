fe5e1ea4159bdb9f9cebcc05580de0b9a7d01378
Zsolt <geczy.zsolt@gmail.com>
2022-02-14 20:29:58 +0100

    Set up logging

--- app/src/main/java/com/gzslt/examplemoviedatabase/ExampleMovieDatabaseApplication.kt
+++ app/src/main/java/com/gzslt/examplemoviedatabase/ExampleMovieDatabaseApplication.kt
@@ -1,5 +1,13 @@
 package com.gzslt.examplemoviedatabase
 
 import android.app.Application
+import logcat.AndroidLogcatLogger
+import logcat.LogPriority
 
-class ExampleMovieDatabaseApplication : Application()
+class ExampleMovieDatabaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidLogcatLogger.installOnDebuggableApp(this, minPriority = LogPriority.VERBOSE)
+    }
+}
