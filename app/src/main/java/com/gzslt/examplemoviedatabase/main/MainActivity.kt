9d5b99025c2cff5bee0e17e2bbe69d12f9bea173
Zsolt <geczy.zsolt@gmail.com>
2022-02-14 20:41:32 +0100

    Set up dependency injection

--- app/src/main/java/com/gzslt/examplemoviedatabase/main/MainActivity.kt
+++ app/src/main/java/com/gzslt/examplemoviedatabase/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.gzslt.examplemoviedatabase
+package com.gzslt.examplemoviedatabase.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gzslt.examplemoviedatabase.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
