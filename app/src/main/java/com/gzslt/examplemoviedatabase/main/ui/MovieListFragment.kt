e9fd78709eb4d7dff777ee7f9dffc2336d58aa4b
Zsolt <geczy.zsolt@gmail.com>
2022-02-15 17:16:54 +0100

    Add search movie field

--- app/src/main/java/com/gzslt/examplemoviedatabase/main/ui/MovieListFragment.kt
+++ app/src/main/java/com/gzslt/examplemoviedatabase/main/ui/MovieListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gzslt.examplemoviedatabase.common.extension.setOnActionSearch
 import com.gzslt.examplemoviedatabase.databinding.FragmentMovieListBinding
 
 class MovieListFragment : BaseFragment() {
@@ -14,10 +15,18 @@ class MovieListFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentMovieListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchTextInputEditText.setOnActionSearch { text ->
+            // TODO
+        }
     }
 
     override fun onDestroyView() {
